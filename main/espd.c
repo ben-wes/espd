1bb21f03a8ee18ea312d4767c23890551cd7ad75
Miller Puckette <msp@ucsd.edu>
2022-03-14 10:33:12 -0700

    temporarily increase buffer size.  This needs to be run-time settable.

--- main/espd.c
+++ main/espd.c
@@ -106,7 +106,7 @@ void app_main(void)
         .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
         .communication_format = I2S_COMM_FORMAT_STAND_I2S,
         .dma_buf_count = 16,
-        .dma_buf_len = 128,
+        .dma_buf_len = 256,
         .use_apll = 1,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2,
     };
