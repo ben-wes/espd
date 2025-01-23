6a0f1703b3893a6d225b6e85e74bdef5c1be2ff6
Miller Puckette <msp@ucsd.edu>
2025-01-23 11:43:00 +0100

    compiler fix

--- arduino/esp32-hal-i2c-slave.c
+++ arduino/esp32-hal-i2c-slave.c
@@ -316,7 +316,7 @@ esp_err_t i2cSlaveInit(uint8_t num, int sda, int scl, uint16_t slaveID, uint32_t
   }
 
   i2c_ll_slave_init(i2c->dev);
-  i2c_ll_slave_set_fifo_mode(i2c->dev, true);
+  i2c_ll_enable_fifo_mode(i2c->dev, true);
   i2c_ll_set_slave_addr(i2c->dev, slaveID, false);
   i2c_ll_set_tout(i2c->dev, I2C_LL_MAX_TIMEOUT);
   i2c_slave_set_frequency(i2c, frequency);
@@ -337,7 +337,7 @@ esp_err_t i2cSlaveInit(uint8_t num, int sda, int scl, uint16_t slaveID, uint32_t
 
   i2c_ll_disable_intr_mask(i2c->dev, I2C_LL_INTR_MASK);
   i2c_ll_clear_intr_mask(i2c->dev, I2C_LL_INTR_MASK);
-  i2c_ll_slave_set_fifo_mode(i2c->dev, true);
+  i2c_ll_enable_fifo_mode(i2c->dev, true);
 
   if (!i2c->intr_handle) {
     uint32_t flags = ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_SHARED;
