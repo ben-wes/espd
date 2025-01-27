e249cd0f2374df7072983ecd3f7bc13ea0bf0e15
Miller Puckette <msp@ucsd.edu>
2025-01-27 11:57:52 +0100

    Revert "add gyro.  Clock speed up to 500 Hz."
    This reverts commit 2d88948c99eef189b70e4974479882060e0e14b6.

--- main/espd.c
+++ main/espd.c
@@ -187,6 +187,7 @@ static void initdacs( void)
     }               
 }
 #else /* OBSOLETEAPI */
+
 static void initdacs( void)
 {
     
@@ -347,9 +348,6 @@ void trymem(int foo);
 
 void app_main(void)
 {
-#ifdef PD_USE_GYRO
-    int have_gyro;
-#endif
     esp_log_level_set("*", ESP_LOG_WARN);
     esp_log_level_set(TAG, ESP_LOG_INFO);
 
@@ -372,24 +370,23 @@ void app_main(void)
     console_init();
 #endif
 
-#ifdef PD_USE_GYRO
-    if (!(have_gyro = gyro_init()))
-        ESP_LOGE(TAG, "gyro init failed");
-    else ESP_LOGI(TAG, "gyro initialized");
-#endif
-
-    ESP_LOGI(TAG, "Pd is running");
+    ESP_LOGI(TAG, "[ 2 ] now write some shit");
 
     while (1)
     {
+        /*
+            int zz = 0;
+            if (!((zz++)%1000))
+            {
+                trymem(5);
+                ESP_LOGI(TAG, "tick");
+            }
+        */
         pd_pollhost();
         pdmain_tick();
         senddacs();
 #ifdef PD_USE_WIFI
         net_alive();
-#endif
-#ifdef PD_USE_GYRO
-        gyro_poll();
 #endif
     }
 }
